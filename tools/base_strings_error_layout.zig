ffb0655a0016b872976ec1217c6999c682222bb2
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-06 11:55:42 -0400

    Cover base string interface layout

--- tools/base_strings_error_layout.zig
+++ tools/base_strings_error_layout.zig
@@ -3,9 +3,17 @@ const base = @import("vst3-zig").pluginterfaces.base;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
+    try printType(stdout, "IStringResult", base.istringresult.IStringResult);
+    try printType(stdout, "IString", base.istringresult.IString);
+    try printType(stdout, "IErrorContext", base.ierrorcontext.IErrorContext);
+
     try printTuid(stdout, "IStringResult", base.istringresult.istring_result_iid);
     try printTuid(stdout, "IString", base.istringresult.istring_iid);
     try printTuid(stdout, "IErrorContext", base.ierrorcontext.ierror_context_iid);
+}
+
+fn printType(writer: anytype, comptime name: []const u8, comptime Type: type) !void {
+    try writer.print("{s} size {} align {}\n", .{ name, @sizeOf(Type), @alignOf(Type) });
 }
 
 fn printTuid(writer: anytype, comptime name: []const u8, bytes: [16]u8) !void {
