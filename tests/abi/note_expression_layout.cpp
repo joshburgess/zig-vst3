ce497d95c1b73cbf19e865a517f907199f6505b7
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-06 01:05:41 -0400

    Expand note expression ABI coverage

--- tests/abi/note_expression_layout.cpp
+++ tests/abi/note_expression_layout.cpp
@@ -18,11 +18,23 @@ static void print_iid (const char* name, const T& tuid)
 int main ()
 {
 	std::printf ("NoteExpressionTypeIDs.kVolumeTypeID %u\n", Steinberg::Vst::kVolumeTypeID);
+	std::printf ("NoteExpressionTypeIDs.kPanTypeID %u\n", Steinberg::Vst::kPanTypeID);
+	std::printf ("NoteExpressionTypeIDs.kTuningTypeID %u\n", Steinberg::Vst::kTuningTypeID);
+	std::printf ("NoteExpressionTypeIDs.kVibratoTypeID %u\n", Steinberg::Vst::kVibratoTypeID);
+	std::printf ("NoteExpressionTypeIDs.kExpressionTypeID %u\n", Steinberg::Vst::kExpressionTypeID);
+	std::printf ("NoteExpressionTypeIDs.kBrightnessTypeID %u\n", Steinberg::Vst::kBrightnessTypeID);
+	std::printf ("NoteExpressionTypeIDs.kTextTypeID %u\n", Steinberg::Vst::kTextTypeID);
+	std::printf ("NoteExpressionTypeIDs.kPhonemeTypeID %u\n", Steinberg::Vst::kPhonemeTypeID);
 	std::printf ("NoteExpressionTypeIDs.kCustomStart %u\n", Steinberg::Vst::kCustomStart);
+	std::printf ("NoteExpressionTypeIDs.kCustomEnd %u\n", Steinberg::Vst::kCustomEnd);
 	std::printf ("NoteExpressionTypeIDs.kInvalidTypeID %u\n", Steinberg::Vst::kInvalidTypeID);
 	std::printf ("NoteExpressionTypeInfo.kIsBipolar %d\n", Steinberg::Vst::NoteExpressionTypeInfo::kIsBipolar);
+	std::printf ("NoteExpressionTypeInfo.kIsOneShot %d\n", Steinberg::Vst::NoteExpressionTypeInfo::kIsOneShot);
+	std::printf ("NoteExpressionTypeInfo.kIsAbsolute %d\n", Steinberg::Vst::NoteExpressionTypeInfo::kIsAbsolute);
 	std::printf ("NoteExpressionTypeInfo.kAssociatedParameterIDValid %d\n", Steinberg::Vst::NoteExpressionTypeInfo::kAssociatedParameterIDValid);
 	std::printf ("KeyswitchTypeIDs.kNoteOnKeyswitchTypeID %u\n", Steinberg::Vst::kNoteOnKeyswitchTypeID);
+	std::printf ("KeyswitchTypeIDs.kOnTheFlyKeyswitchTypeID %u\n", Steinberg::Vst::kOnTheFlyKeyswitchTypeID);
+	std::printf ("KeyswitchTypeIDs.kOnReleaseKeyswitchTypeID %u\n", Steinberg::Vst::kOnReleaseKeyswitchTypeID);
 	std::printf ("KeyswitchTypeIDs.kKeyRangeTypeID %u\n", Steinberg::Vst::kKeyRangeTypeID);
 
 	PRINT_TYPE (NoteExpressionValueDescription);
@@ -46,6 +58,9 @@ int main ()
 	PRINT_OFFSET (KeyswitchInfo, title);
 	PRINT_OFFSET (KeyswitchInfo, shortTitle);
 	PRINT_OFFSET (KeyswitchInfo, keyswitchMin);
+	PRINT_OFFSET (KeyswitchInfo, keyswitchMax);
+	PRINT_OFFSET (KeyswitchInfo, keyRemapped);
+	PRINT_OFFSET (KeyswitchInfo, unitId);
 	PRINT_OFFSET (KeyswitchInfo, flags);
 
 	print_iid ("INoteExpressionController", Steinberg::Vst::INoteExpressionController_iid);
