699bd9033af5e848377df57c76d567de41b0d401
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-06 12:01:54 -0400

    Cover IBStream interface layout

--- tools/ibstream_layout.zig
+++ tools/ibstream_layout.zig
@@ -3,11 +3,17 @@ const ibstream = @import("vst3-zig").pluginterfaces.base.ibstream;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
+    try printType(stdout, "Steinberg::IBStream", ibstream.IBStream);
+    try printType(stdout, "Steinberg::ISizeableStream", ibstream.ISizeableStream);
     try stdout.print("IBStream.kIBSeekSet {}\n", .{@intFromEnum(ibstream.IStreamSeekMode.kIBSeekSet)});
     try stdout.print("IBStream.kIBSeekCur {}\n", .{@intFromEnum(ibstream.IStreamSeekMode.kIBSeekCur)});
     try stdout.print("IBStream.kIBSeekEnd {}\n", .{@intFromEnum(ibstream.IStreamSeekMode.kIBSeekEnd)});
     try printTuid(stdout, "IBStream", ibstream.ibstream_iid);
     try printTuid(stdout, "ISizeableStream", ibstream.isizeable_stream_iid);
+}
+
+fn printType(writer: anytype, comptime name: []const u8, comptime T: type) !void {
+    try writer.print("{s} size {} align {}\n", .{ name, @sizeOf(T), @alignOf(T) });
 }
 
 fn printTuid(writer: anytype, comptime name: []const u8, bytes: [16]u8) !void {
