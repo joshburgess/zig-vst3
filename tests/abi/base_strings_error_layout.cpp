c9f50ff2dd41934303a57fe57246c8c4af47fef9
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-06 11:55:42 -0400

    Cover base string interface layout

--- tests/abi/base_strings_error_layout.cpp
+++ tests/abi/base_strings_error_layout.cpp
@@ -1,7 +1,10 @@
 #include "pluginterfaces/base/ierrorcontext.h"
 #include "pluginterfaces/base/istringresult.h"
 
+#include <cstddef>
 #include <cstdio>
+
+#define PRINT_TYPE(Type) std::printf (#Type " size %zu align %zu\n", sizeof (Steinberg::Type), alignof (Steinberg::Type))
 
 template <typename T>
 static void print_iid (const char* name, const T& tuid)
@@ -14,6 +17,10 @@ static void print_iid (const char* name, const T& tuid)
 
 int main ()
 {
+	PRINT_TYPE (IStringResult);
+	PRINT_TYPE (IString);
+	PRINT_TYPE (IErrorContext);
+
 	print_iid ("IStringResult", Steinberg::IStringResult_iid);
 	print_iid ("IString", Steinberg::IString_iid);
 	print_iid ("IErrorContext", Steinberg::IErrorContext_iid);
