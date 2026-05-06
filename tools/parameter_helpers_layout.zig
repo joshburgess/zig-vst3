4c8a458c930a502cd0f93190782a59d863476108
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-06 00:50:50 -0400

    Expand parameter function ABI coverage

--- tools/parameter_helpers_layout.zig
+++ tools/parameter_helpers_layout.zig
@@ -6,10 +6,15 @@ const remap = @import("vst3-zig").pluginterfaces.vst.ivstremapparamid;
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("FunctionNameType.kCompGainReduction {s}\n", .{std.mem.span(function_name.FunctionNameType.kCompGainReduction)});
+    try stdout.print("FunctionNameType.kCompGainReductionMax {s}\n", .{std.mem.span(function_name.FunctionNameType.kCompGainReductionMax)});
+    try stdout.print("FunctionNameType.kCompGainReductionPeakHold {s}\n", .{std.mem.span(function_name.FunctionNameType.kCompGainReductionPeakHold)});
+    try stdout.print("FunctionNameType.kCompResetGainReductionMax {s}\n", .{std.mem.span(function_name.FunctionNameType.kCompResetGainReductionMax)});
     try stdout.print("FunctionNameType.kLowLatencyMode {s}\n", .{std.mem.span(function_name.FunctionNameType.kLowLatencyMode)});
     try stdout.print("FunctionNameType.kDryWetMix {s}\n", .{std.mem.span(function_name.FunctionNameType.kDryWetMix)});
     try stdout.print("FunctionNameType.kRandomize {s}\n", .{std.mem.span(function_name.FunctionNameType.kRandomize)});
     try stdout.print("FunctionNameType.kPanPosCenterX {s}\n", .{std.mem.span(function_name.FunctionNameType.kPanPosCenterX)});
+    try stdout.print("FunctionNameType.kPanPosCenterY {s}\n", .{std.mem.span(function_name.FunctionNameType.kPanPosCenterY)});
+    try stdout.print("FunctionNameType.kPanPosCenterZ {s}\n", .{std.mem.span(function_name.FunctionNameType.kPanPosCenterZ)});
 
     try printTuid(stdout, "IParameterFunctionName", function_name.iparameter_function_name_iid);
     try printTuid(stdout, "IParameterFinder", parameter_finder.iparameter_finder_iid);
