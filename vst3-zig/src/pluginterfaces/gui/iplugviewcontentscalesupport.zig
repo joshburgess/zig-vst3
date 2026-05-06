1c37c7fabf4088eec4b03af4f822832e36bff0db
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-06 16:16:22 -0400

    Assert plug view scale support size

--- vst3-zig/src/pluginterfaces/gui/iplugviewcontentscalesupport.zig
+++ vst3-zig/src/pluginterfaces/gui/iplugviewcontentscalesupport.zig
@@ -17,5 +17,6 @@ pub const IPlugViewContentScaleSupport = extern struct {
 };
 
 test "plug view scale support vtable slot count includes FUnknown prefix" {
+    try @import("std").testing.expectEqual(@as(usize, @sizeOf(usize)), @sizeOf(IPlugViewContentScaleSupport));
     try @import("std").testing.expectEqual(@as(usize, 4), @typeInfo(IPlugViewContentScaleSupportVTable).@"struct".fields.len);
 }
