3c6ba68f9948e71fb27a4e9d618f04b5070231a5
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-07 10:12:23 -0400

    Add reusable update handler

--- vst3-zig/src/root.zig
+++ vst3-zig/src/root.zig
@@ -39,6 +39,7 @@ pub const vst_parameter_finder = @import("vst_parameter_finder.zig");
 pub const vst_persistent_attributes = @import("vst_persistent_attributes.zig");
 pub const vst_plugin_compatibility = @import("vst_plugin_compatibility.zig");
 pub const vst_string_result = @import("vst_string_result.zig");
+pub const vst_update_handler = @import("vst_update_handler.zig");
 pub const vst_wayland_frame = @import("vst_wayland_frame.zig");
 pub const voice_mix_component = @import("voice_mix_component.zig");
 pub const voice_mix_controller = @import("voice_mix_controller.zig");
