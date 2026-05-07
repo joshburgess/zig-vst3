35eb43187de809f25f4daef84e457faf14ce0b35
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-07 03:08:13 -0400

    Add reusable VST message objects

--- vst3-zig/src/root.zig
+++ vst3-zig/src/root.zig
@@ -32,6 +32,7 @@ pub const pluginterfaces = struct {
 };
 pub const tuid = @import("tuid.zig");
 pub const version = "0.1.0-dev";
+pub const vst_message = @import("vst_message.zig");
 pub const voice_mix_component = @import("voice_mix_component.zig");
 pub const voice_mix_controller = @import("voice_mix_controller.zig");
 pub const voice_mix_plugin = @import("voice_mix_plugin.zig");
