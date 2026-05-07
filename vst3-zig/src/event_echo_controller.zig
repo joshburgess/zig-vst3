a6b186340e2326b5f8199c6dda0a3326f0ccf125
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-07 02:06:23 -0400

    Add bundled event echo example

---
+++ vst3-zig/src/event_echo_controller.zig
@@ -0,0 +1,41 @@
+const event_echo_spec = @import("event_echo_spec.zig");
+const ibstream = @import("pluginterfaces/base/ibstream.zig");
+const plug_process = @import("zig-plug-core").process;
+const tuid = @import("tuid.zig");
+const types = @import("pluginterfaces/base/types.zig");
+const zig_plug_effect = @import("zig_plug_effect.zig");
+
+pub const cid = tuid.inlineUid(0x33C5AD3E, 0xAA9741CE, 0xA10192B6, 0x420DE47A);
+
+const Controller = zig_plug_effect.ReflectedEditController(struct {
+    pub const controller_name = "EventEchoController";
+    pub const Params = event_echo_spec.Spec.Params;
+    pub const parameter_set = &event_echo_spec.parameter_set;
+});
+
+pub const create = Controller.create;
+
+pub fn applyParameterChanges(changes: plug_process.ParameterChanges) void {
+    Controller.applyParameterChanges(changes);
+}
+
+pub fn readState(state: ?*ibstream.IBStream) types.tresult {
+    return Controller.readState(state);
+}
+
+pub fn writeState(state: ?*ibstream.IBStream) types.tresult {
+    return Controller.writeState(state);
+}
+
+test "event echo controller can be created as IEditController" {
+    const std = @import("std");
+    const ivsteditcontroller = @import("pluginterfaces/vst/ivsteditcontroller.zig");
+
+    var out: ?*anyopaque = null;
+
+    try std.testing.expectEqual(types.kResultOk, create(@ptrCast(&ivsteditcontroller.iedit_controller_iid), &out));
+    try std.testing.expect(out != null);
+    const controller_iface: *ivsteditcontroller.IEditController = @ptrCast(@alignCast(out.?));
+    try std.testing.expectEqual(@as(types.int32, 0), controller_iface.vtable.getParameterCount(controller_iface));
+    try std.testing.expect(controller_iface.vtable.release(controller_iface) >= 1);
+}
