005cfe606f2175909eadbc1abb25fe86206a64b4
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-08 23:58:43 -0400

    Use block segments in gain example

--- examples/gain_core.zig
+++ examples/gain_core.zig
@@ -9,12 +9,29 @@ pub const Gain = struct {
     };
 
     pub fn process(_: *Gain, context: *plug.process.ProcessContext(f32)) void {
-        for (0..context.outputChannelCount()) |channel| {
-            const input = context.inputChannel(channel) orelse continue;
-            const output = context.outputChannel(channel) orelse continue;
-            for (0..context.frameCount()) |sample| {
-                const gain = context.parameterNormalizedAtOrBeforeOr(0, sample, 1.0);
-                output[sample] = input[sample] * @as(f32, @floatCast(gain));
+        var segments = context.parameterBlockSegments();
+        while (segments.next()) |segment| {
+            const gain = @as(f32, @floatCast(context.parameterNormalizedAtOrBeforeOr(0, segment.start_offset, 1.0)));
+            for (0..context.outputChannelCount()) |channel| {
+                const input = context.inputChannel(channel) orelse continue;
+                const output = context.outputChannel(channel) orelse continue;
+                for (segment.start_offset..segment.end_offset) |sample| {
+                    output[sample] = input[sample] * gain;
+                }
+            }
+        }
+    }
+
+    pub fn process64(_: *Gain, context: *plug.process.ProcessContext(f64)) void {
+        var segments = context.parameterBlockSegments();
+        while (segments.next()) |segment| {
+            const gain = context.parameterNormalizedAtOrBeforeOr(0, segment.start_offset, 1.0);
+            for (0..context.outputChannelCount()) |channel| {
+                const input = context.inputChannel(channel) orelse continue;
+                const output = context.outputChannel(channel) orelse continue;
+                for (segment.start_offset..segment.end_offset) |sample| {
+                    output[sample] = input[sample] * gain;
+                }
             }
         }
     }
@@ -54,6 +71,48 @@ test "gain core example processes through zig-plug context" {
     try std.testing.expectEqual(@as(f32, 0.125), output[0]);
     try std.testing.expectEqual(@as(f32, 0.25), output[1]);
     try std.testing.expectEqual(@as(f32, 0.5), output[2]);
+}
+
+test "gain core example splits blocks at automation changes" {
+    var plugin = Gain{};
+    const input = [_]f32{ 1.0, 1.0, 1.0, 1.0, 1.0 };
+    var output = [_]f32{ 0.0, 0.0, 0.0, 0.0, 0.0 };
+    const input_channels = [_][]const f32{&input};
+    const output_channels = [_][]f32{&output};
+    const changes = [_]plug.process.ParameterChange{
+        parameter_set.parameterChange("gain", 1, 0.5),
+        parameter_set.parameterChange("gain", 3, 0.25),
+    };
+    var context = try plug.process.ProcessContext(f32).initWith(48_000.0, &input_channels, &output_channels, .{
+        .parameter_changes = &changes,
+    });
+
+    plugin.process(&context);
+
+    try std.testing.expectEqual(@as(f32, 1.0), output[0]);
+    try std.testing.expectEqual(@as(f32, 0.5), output[1]);
+    try std.testing.expectEqual(@as(f32, 0.5), output[2]);
+    try std.testing.expectEqual(@as(f32, 0.25), output[3]);
+    try std.testing.expectEqual(@as(f32, 0.25), output[4]);
+}
+
+test "gain core example supports double precision processing" {
+    var instance = try Instance.init(std.testing.allocator, .{});
+    const input = [_]f64{ 0.25, 0.5 };
+    var output = [_]f64{ 0.0, 0.0 };
+    const input_channels = [_][]const f64{&input};
+    const output_channels = [_][]f64{&output};
+    const changes = [_]plug.process.ParameterChange{
+        parameter_set.parameterChange("gain", 0, 0.5),
+    };
+    var context = try plug.process.ProcessContext(f64).initWith(48_000.0, &input_channels, &output_channels, .{
+        .parameter_changes = &changes,
+    });
+
+    instance.process64(&context);
+
+    try std.testing.expectEqual(@as(f64, 0.125), output[0]);
+    try std.testing.expectEqual(@as(f64, 0.25), output[1]);
 }
 
 test "gain core example can run through plugin instance" {
