a7550d1e87153b3da7d48c3c52989e24cde8d2ca
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-10 00:01:32 -0400

    Make note gate sample-offset aware

--- examples/note_gate_core.zig
+++ examples/note_gate_core.zig
@@ -6,16 +6,33 @@ pub const NoteGate = struct {
     pub const vendor = "zig-vst3";
     pub const Params = struct {};
 
-    pub fn process(_: *NoteGate, context: *plug.process.ProcessContext(f32)) void {
-        if (!context.hasEvent(.note_on)) {
-            context.clearOutputs();
-            return;
+    open: bool = false,
+
+    pub fn process(self: *NoteGate, context: *plug.process.ProcessContext(f32)) void {
+        var segments = context.inputEventBlockSegments();
+        while (segments.next()) |segment| {
+            self.applyEventsAt(context, segment.start_offset);
+            for (0..context.outputChannelCount()) |channel| {
+                const input = context.inputChannel(channel) orelse continue;
+                const output = context.outputChannel(channel) orelse continue;
+                if (self.open) {
+                    for (segment.start_offset..segment.end_offset) |sample| {
+                        output[sample] = input[sample];
+                    }
+                } else {
+                    @memset(output[segment.start_offset..segment.end_offset], 0.0);
+                }
+            }
         }
-        for (0..context.outputChannelCount()) |channel| {
-            const input = context.inputChannel(channel) orelse continue;
-            const output = context.outputChannel(channel) orelse continue;
-            for (0..context.frameCount()) |sample| {
-                output[sample] = input[sample];
+    }
+
+    fn applyEventsAt(self: *NoteGate, context: *plug.process.ProcessContext(f32), sample_offset: usize) void {
+        var events = context.inputEventsAtOffset(sample_offset);
+        while (events.next()) |event| {
+            switch (event.kind) {
+                .note_on => self.open = event.velocity > 0.0,
+                .note_off => self.open = false,
+                else => {},
             }
         }
     }
@@ -61,9 +78,33 @@ test "note gate core example passes audio when a note-on event is present" {
 
     plugin.process(&context);
 
-    try std.testing.expectEqual(@as(f32, 0.25), output[0]);
+    try std.testing.expectEqual(@as(f32, 0.0), output[0]);
     try std.testing.expectEqual(@as(f32, -0.5), output[1]);
     try std.testing.expectEqual(@as(f32, 1.0), output[2]);
+    try std.testing.expect(plugin.open);
+}
+
+test "note gate core example closes on note-off inside a block" {
+    var plugin = NoteGate{};
+    const input = [_]f32{ 0.25, -0.5, 1.0, -1.0 };
+    var output = [_]f32{ 1.0, 1.0, 1.0, 1.0 };
+    const input_channels = [_][]const f32{&input};
+    const output_channels = [_][]f32{&output};
+    const events = [_]plug.process.Event{
+        plug.process.Event.noteOn(1, 0, 60, 0.75),
+        plug.process.Event.noteOff(3, 0, 60, 0.0),
+    };
+    var context = try plug.process.ProcessContext(f32).initWith(48_000.0, &input_channels, &output_channels, .{
+        .events = &events,
+    });
+
+    plugin.process(&context);
+
+    try std.testing.expectEqual(@as(f32, 0.0), output[0]);
+    try std.testing.expectEqual(@as(f32, -0.5), output[1]);
+    try std.testing.expectEqual(@as(f32, 1.0), output[2]);
+    try std.testing.expectEqual(@as(f32, 0.0), output[3]);
+    try std.testing.expect(!plugin.open);
 }
 
 test "note gate core example can run through plugin instance" {
@@ -83,4 +124,5 @@ test "note gate core example can run through plugin instance" {
 
     try std.testing.expectEqual(@as(f32, 0.25), output[0]);
     try std.testing.expectEqual(@as(f32, -0.5), output[1]);
+    try std.testing.expect(instance.plugin.open);
 }
