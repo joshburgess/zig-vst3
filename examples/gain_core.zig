e42933b5b44bb5846abe573b69ecacca9cd45498
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-10 22:10:23 -0400

    Use state classification predicates in gain example

--- examples/gain_core.zig
+++ examples/gain_core.zig
@@ -219,6 +219,8 @@ test "gain core example round-trips parameter state" {
     try std.testing.expect(!report.hasIgnoredEntries());
     try std.testing.expect(report.fullyHandled());
     try std.testing.expectEqual(plug.state.ReadParameterStateClassification.restored_all, report.classification());
+    try std.testing.expect(report.isRestoredAllClassification());
+    try std.testing.expect(!report.isPartialClassification());
     try std.testing.expect(report.restoredAllEntries());
     try std.testing.expect(!report.ignoredAllEntries());
     try std.testing.expectEqual(@as(f64, 0.25), restored.loadParameterNormalized("gain"));
