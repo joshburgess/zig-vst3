b86a322b121031fda647613059479d05c7f42c98
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-11 03:10:14 -0400

    Exercise prepare validation in gain example

--- examples/gain_core.zig
+++ examples/gain_core.zig
@@ -58,6 +58,11 @@ test "gain core example declares reflected metadata" {
     try std.testing.expect(instance.hasProcess64Hook());
     try std.testing.expect(instance.hasAnyProcessHook());
     try std.testing.expect(!instance.hasDeinitHook());
+    try (plug.plugin.PrepareConfig{ .sample_rate = 48_000.0, .max_block_size = 64 }).validate();
+    try std.testing.expectError(error.InvalidSampleRate, (plug.plugin.PrepareConfig{ .sample_rate = 0.0, .max_block_size = 64 }).validate());
+    try std.testing.expectError(error.InvalidMaxBlockSize, (plug.plugin.PrepareConfig{ .sample_rate = 48_000.0, .max_block_size = 0 }).validate());
+    try instance.prepareChecked(.{ .sample_rate = 48_000.0, .max_block_size = 64 });
+    try std.testing.expectError(error.InvalidSampleRate, instance.prepareChecked(.{ .sample_rate = std.math.nan(f64), .max_block_size = 64 }));
     try std.testing.expectEqual(@as(usize, 1), parameter_set.parameterCount());
     try std.testing.expectEqualStrings("Gain", parameter_set.shortName(0).?);
     try std.testing.expectEqualStrings("x", parameter_set.units(0).?);
