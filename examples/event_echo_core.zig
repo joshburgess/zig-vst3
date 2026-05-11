a31e24e8365a59907b3efde46d23884b6c0e9d4c
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-11 02:42:07 -0400

    Exercise output event routing helpers

--- examples/event_echo_core.zig
+++ examples/event_echo_core.zig
@@ -30,9 +30,10 @@ test "event echo core example writes input events to output events" {
     const input_channels = [_][]const f32{&input};
     const output_channels = [_][]f32{&output};
     const events = [_]plug.process.Event{
-        plug.process.Event.noteOn(1, 0, 60, 0.75),
+        plug.process.Event.noteOn(0, 0, 60, 0.75),
+        plug.process.Event.noteOff(1, 0, 60, 0.0),
     };
-    var output_event_storage: [1]plug.process.Event = undefined;
+    var output_event_storage: [2]plug.process.Event = undefined;
     var output_events = plug.process.EventWriter.init(&output_event_storage, input.len);
     var context = try plug.process.ProcessContext(f32).initWith(48_000.0, &input_channels, &output_channels, .{
         .events = &events,
@@ -44,33 +45,77 @@ test "event echo core example writes input events to output events" {
     try std.testing.expect(context.hasOutputEventWriter());
     try std.testing.expect(!context.outputEventsEmpty());
     try std.testing.expect(context.outputEventsFull());
-    try std.testing.expectEqual(@as(usize, 1), context.outputEventCapacity());
-    try std.testing.expectEqual(@as(usize, 1), context.outputEventCount());
+    try std.testing.expectEqual(@as(usize, 2), context.outputEventCapacity());
+    try std.testing.expectEqual(@as(usize, 2), context.outputEventCount());
     try std.testing.expectEqual(@as(usize, 0), context.outputEventRemainingCapacity());
     try std.testing.expectEqual(@as(usize, input.len), context.outputEventFrameCount());
     try std.testing.expect(context.hasOutputEvent(.note_on));
+    try std.testing.expect(context.hasOutputEvent(.note_off));
     try std.testing.expect(context.hasOutputNoteAttacks());
-    try std.testing.expect(!context.hasOutputNoteReleases());
+    try std.testing.expect(context.hasOutputNoteReleases());
     try std.testing.expectEqual(@as(usize, 1), context.countOutputEvents(.note_on));
+    try std.testing.expectEqual(@as(usize, 1), context.countOutputEvents(.note_off));
     try std.testing.expectEqual(@as(usize, 1), context.countOutputNoteAttacks());
-    try std.testing.expectEqual(@as(usize, 0), context.countOutputNoteReleases());
-    try std.testing.expect(context.onlyOutputNoteAttacks());
+    try std.testing.expectEqual(@as(usize, 1), context.countOutputNoteReleases());
+    try std.testing.expect(!context.onlyOutputEventsOfKind(.note_on));
+    try std.testing.expect(!context.onlyOutputNoteAttacks());
+    try std.testing.expect(!context.onlyOutputNoteReleases());
+    try std.testing.expect(context.hasOutputEventsForBus(0));
+    try std.testing.expect(context.hasOutputEventsForChannel(0));
+    try std.testing.expect(context.hasOutputEventsForBusChannel(0, 0));
+    try std.testing.expect(context.onlyOutputEventsForBus(0));
+    try std.testing.expect(context.onlyOutputEventsForChannel(0));
+    try std.testing.expect(context.onlyOutputEventsForBusChannel(0, 0));
+    try std.testing.expect(!context.hasOutputEventsForBus(1));
+    try std.testing.expect(context.outputEventsForBusEmpty(1));
+    try std.testing.expectEqual(@as(usize, 2), context.countOutputEventsForBus(0));
+    try std.testing.expectEqual(@as(usize, 2), context.countOutputEventsForChannel(0));
+    try std.testing.expectEqual(@as(usize, 2), context.countOutputEventsForBusChannel(0, 0));
+    try std.testing.expectEqual(@as(usize, 0), context.countOutputEventsForBusChannel(1, 0));
     try std.testing.expectEqual(plug.process.EventKind.note_on, context.firstWrittenOutputEvent().?.kind);
-    try std.testing.expectEqual(plug.process.EventKind.note_on, context.latestWrittenOutputEvent().?.kind);
-    try std.testing.expectEqual(@as(usize, 1), context.firstOutputEvent(.note_on).?.sample_offset);
-    try std.testing.expectEqual(@as(usize, 1), context.latestOutputEvent(.note_on).?.sample_offset);
-    try std.testing.expectEqual(@as(usize, 1), context.firstOutputEventOffset().?);
-    try std.testing.expectEqual(@as(usize, 1), context.latestOutputEventOffset().?);
-    try std.testing.expectEqual(@as(usize, 1), context.firstOutputEventOffsetForKind(.note_on).?);
-    try std.testing.expectEqual(@as(usize, 1), context.latestOutputEventOffsetForKind(.note_on).?);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, context.latestWrittenOutputEvent().?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_on, context.firstOutputEventAtOffset(0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, context.latestOutputEventAtOffset(1).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_on, context.firstOutputEventOfKindAtOffset(.note_on, 0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, context.latestOutputEventOfKindAtOffset(.note_off, 1).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_on, context.firstOutputEventForBus(0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, context.latestOutputEventForBus(0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_on, context.firstOutputEventForChannel(0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, context.latestOutputEventForChannel(0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_on, context.firstOutputEventForBusChannel(0, 0).?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, context.latestOutputEventForBusChannel(0, 0).?.kind);
+    try std.testing.expectEqual(@as(usize, 0), context.firstOutputEvent(.note_on).?.sample_offset);
+    try std.testing.expectEqual(@as(usize, 1), context.latestOutputEvent(.note_off).?.sample_offset);
+    try std.testing.expectEqual(@as(?usize, 0), context.firstOutputEventOffset());
+    try std.testing.expectEqual(@as(?usize, 1), context.latestOutputEventOffset());
+    try std.testing.expectEqual(@as(?usize, 0), context.firstOutputEventOffsetForKind(.note_on));
+    try std.testing.expectEqual(@as(?usize, 1), context.latestOutputEventOffsetForKind(.note_off));
+    try std.testing.expectEqual(@as(?usize, 0), context.firstOutputEventOffsetForBus(0));
+    try std.testing.expectEqual(@as(?usize, 1), context.latestOutputEventOffsetForBus(0));
+    try std.testing.expectEqual(@as(?usize, 0), context.firstOutputEventOffsetForChannel(0));
+    try std.testing.expectEqual(@as(?usize, 1), context.latestOutputEventOffsetForChannel(0));
+    try std.testing.expectEqual(@as(?usize, 0), context.firstOutputEventOffsetForBusChannel(0, 0));
+    try std.testing.expectEqual(@as(?usize, 1), context.latestOutputEventOffsetForBusChannel(0, 0));
+    try std.testing.expectEqual(@as(?usize, null), context.firstOutputEventOffsetForBus(1));
+    try std.testing.expectEqual(@as(?plug.process.Event, null), context.firstOutputEventForBus(1));
+    try std.testing.expectEqual(@as(?plug.process.Event, null), context.latestOutputEventForChannel(1));
+    try std.testing.expectEqual(@as(?usize, 1), context.nextOutputEventOffset(0));
     try std.testing.expectEqual(@as(?usize, null), context.nextOutputEventOffset(1));
+    try std.testing.expectEqual(@as(?usize, 1), context.nextOutputEventOffsetForKind(.note_off, 0));
+    try std.testing.expectEqual(@as(?usize, null), context.nextOutputEventOffsetForKind(.note_on, 0));
+    try std.testing.expectEqual(@as(?usize, 1), context.nextOutputEventOffsetForBus(0, 0));
+    try std.testing.expectEqual(@as(?usize, 1), context.nextOutputEventOffsetForChannel(0, 0));
+    try std.testing.expectEqual(@as(?usize, 1), context.nextOutputEventOffsetForBusChannel(0, 0, 0));
+    try std.testing.expectEqual(@as(?usize, null), context.nextOutputEventOffsetForBus(1, 0));
+    try std.testing.expect(!context.canAppendOutputEvent());
+    try std.testing.expect(!context.canAppendOutputEventValue(events[0]));
 
     var output_events_at_offset = context.outputEventsAtOffset(1);
-    try std.testing.expectEqual(plug.process.EventKind.note_on, output_events_at_offset.next().?.kind);
+    try std.testing.expectEqual(plug.process.EventKind.note_off, output_events_at_offset.next().?.kind);
     try std.testing.expectEqual(@as(?plug.process.Event, null), output_events_at_offset.next());
 
     var output_note_events = context.outputEventsOfKind(.note_on);
-    try std.testing.expectEqual(@as(usize, 1), output_note_events.next().?.sample_offset);
+    try std.testing.expectEqual(@as(usize, 0), output_note_events.next().?.sample_offset);
     try std.testing.expectEqual(@as(?plug.process.Event, null), output_note_events.next());
 
     var segments = context.outputEventBlockSegments();
@@ -78,11 +123,20 @@ test "event echo core example writes input events to output events" {
     try std.testing.expectEqual(plug.process.BlockSegment{ .start_offset = 1, .end_offset = 2 }, segments.next().?);
     try std.testing.expectEqual(@as(?plug.process.BlockSegment, null), segments.next());
 
-    context.clearOutputEvents();
+    try std.testing.expectEqual(@as(usize, 2), context.clearOutputEventsCount());
+    try std.testing.expectEqual(@as(usize, 0), context.clearOutputEventsCount());
     try std.testing.expect(context.outputEventsEmpty());
+    try std.testing.expect(!context.hasOutputEvents());
     try std.testing.expect(!context.outputEventsFull());
     try std.testing.expectEqual(@as(usize, 0), context.outputEventCount());
-    try std.testing.expectEqual(@as(usize, 1), context.outputEventRemainingCapacity());
+    try std.testing.expectEqual(@as(usize, 2), context.outputEventRemainingCapacity());
+    try std.testing.expectEqual(@as(?usize, null), context.firstOutputEventOffset());
+    try std.testing.expectEqual(@as(?usize, null), context.latestOutputEventOffset());
+    try std.testing.expectEqual(@as(?plug.process.Event, null), context.firstWrittenOutputEvent());
+    try std.testing.expectEqual(@as(?plug.process.Event, null), context.latestWrittenOutputEvent());
+    try std.testing.expect(!context.hasOutputEvent(.note_on));
+    try std.testing.expect(!context.hasOutputNoteAttacks());
+    try std.testing.expect(!context.hasOutputNoteReleases());
 }
 
 test "event echo core example leaves output unchanged when capacity is too small" {
