142ed4314fc7b3d40d19d4de14884263e4540c29
Josh Burgess <joshburgesswebdev@gmail.com>
2026-07-29 17:53:21 -0400

    Link Linux GUI runtimes as libraries

--- build.zig
+++ build.zig
@@ -4358,8 +4358,9 @@ fn addVstguiAdapter(module: *std.Build.Module, target: std.Build.ResolvedTarget)
         module.linkFramework("Accelerate", .{});
         module.linkFramework("UniformTypeIdentifiers", .{});
     } else if (target.result.os.tag == .linux) {
-        module.addObjectFile(b.path(".vst3-sdk/vstgui-adapter-build/Release/libs/libstdc++.so"));
-        module.addObjectFile(b.path(".vst3-sdk/vstgui-adapter-build/Release/libs/libgcc_s.so.1"));
+        module.addLibraryPath(b.path(".vst3-sdk/vstgui-adapter-build/Release/libs"));
+        module.linkSystemLibrary(":libstdc++.so", .{ .use_pkg_config = .no });
+        module.linkSystemLibrary(":libgcc_s.so.1", .{ .use_pkg_config = .no });
         for ([_][]const u8{
             "X11",            "freetype2",      "xcb",         "xcb-util",       "xcb-cursor", "xcb-keysyms", "xcb-xkb",
             "xkbcommon",      "xkbcommon-x11",  "glib-2.0",    "cairo",          "pangocairo", "pangoft2",    "fontconfig",
